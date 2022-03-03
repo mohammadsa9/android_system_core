master-hax/aosp-system-core

healthd: Make initHealthInfo() easier to read
Use aggregate initialization instead of memberwise initialization. This
patch does not change any functionality.

Test: Compile-tested only.
Change-Id: I9c7831936e449aec8a48336b1ee76d26638e4f72
Signed-off-by: Bart Van Assche <bvanassche@google.com>

healthd/BatteryMonitor.cpp
@@ -75,16 +75,14 @@ static std::optional<T> mapSysfsString(const char* str, SysfsStringEnumMap<T> ma
 }
 
 static void initHealthInfo(HealthInfo_2_1* health_info_2_1) {
-    *health_info_2_1 = HealthInfo_2_1{};
-
-    // HIDL enum values are zero initialized, so they need to be initialized
-    // properly.
-    health_info_2_1->batteryCapacityLevel = BatteryCapacityLevel::UNSUPPORTED;
-    health_info_2_1->batteryChargeTimeToFullNowSeconds =
-            (int64_t)Constants::BATTERY_CHARGE_TIME_TO_FULL_NOW_SECONDS_UNSUPPORTED;
-    auto* props = &health_info_2_1->legacy.legacy;
-    props->batteryStatus = BatteryStatus::UNKNOWN;
-    props->batteryHealth = BatteryHealth::UNKNOWN;
+    *health_info_2_1 = {
+            .batteryCapacityLevel = BatteryCapacityLevel::UNSUPPORTED,
+            .batteryChargeTimeToFullNowSeconds =
+                    (int64_t)Constants::BATTERY_CHARGE_TIME_TO_FULL_NOW_SECONDS_UNSUPPORTED,
+            .legacy.legacy = {
+                    .batteryStatus = BatteryStatus::UNKNOWN,
+                    .batteryHealth = BatteryHealth::UNKNOWN,
+            }};
 }
 
 BatteryMonitor::BatteryMonitor()

mohammadsa9/android_system_core

healthd/BatteryMonitor.cpp
@@ -121,14 +121,13 @@ static std::optional<T> mapSysfsString(const char* str, SysfsStringEnumMap<T> ma
 }
 
 static void initHealthInfo(HealthInfo* health_info) {
-    *health_info = HealthInfo{};
-
-    // Enum values may be zero initialized, so they need to be initialized properly.
-    health_info->batteryCapacityLevel = BatteryCapacityLevel::UNSUPPORTED;
-    health_info->batteryChargeTimeToFullNowSeconds =
-            (int64_t)HealthInfo::BATTERY_CHARGE_TIME_TO_FULL_NOW_SECONDS_UNSUPPORTED;
-    health_info->batteryStatus = BatteryStatus::UNKNOWN;
-    health_info->batteryHealth = BatteryHealth::UNKNOWN;
+    *health_info = {
+            .batteryCapacityLevel = BatteryCapacityLevel::UNSUPPORTED,
+            .batteryChargeTimeToFullNowSeconds =
+                    (int64_t)HealthInfo::BATTERY_CHARGE_TIME_TO_FULL_NOW_SECONDS_UNSUPPORTED,
+            .batteryStatus = BatteryStatus::UNKNOWN,
+            .batteryHealth = BatteryHealth::UNKNOWN,
+    };
 }
 
 BatteryMonitor::BatteryMonitor()